c987841f77f52ec36c96494ab304d6e1ce4b9dbd
Saikat Sharma <saikatsharma4@gmail.com>
2018-05-12 02:02:11 +0600

    Codeforces problem solutions added

---
+++ Codeforces/919/B. Perfect Number.cpp
@@ -0,0 +1,84 @@
+/***************************************************
+ * Problem name : B. Perfect Number.cpp
+ * OJ           : Codeforces
+ * Verdict      : AC
+ * Date         : 2018-02-14
+ * Problem Type : Div 2 - B
+ * Author Name  : Saikat Sharma
+ * University   : CSE,MBSTU
+ ***************************************************/
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<climits>
+#include<cstring>
+#include<string>
+#include<sstream>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<cstdlib>
+#include<deque>
+#include<stack>
+#include<map>
+#include<set>
+#define __FastIO ios_base::sync_with_stdio(false); cin.tie(0)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define pii pair<int,int>
+#define pll pair <int, int>
+#define debug printf("#########\n")
+#define nl printf("\n")
+#define sp printf(" ")
+#define sl(n) scanf("%lld", &n)
+#define sf(n) scanf("%lf", &n)
+#define si(n) scanf("%d", &n)
+#define ss(n) scanf("%s", n)
+#define pf(n) scanf("%d", n)
+#define pfl(n) scanf("%lld", n)
+#define pb push_back
+#define MAX 10005
+#define INF 1000000000
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+template <typename T>
+std::string NumberToString ( T Number ) {
+    std::ostringstream ss;
+    ss << Number;
+    return ss.str();
+}
+ll gcd(ll a, ll b) {
+    if (a % b == 0) return b;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+    return a * b / gcd(a, b);
+}
+/************************************ Code Start Here ******************************************************/
+vector<ll>P;
+ll Sum(ll n) {
+    ll sum = 0;
+    while (n > 0) {
+        ll rem = n % 10;
+        n /= 10;
+        sum += rem;
+    }
+    return sum;
+}
+void fun() {
+    for (int i = 19, j = 0; j <= MAX; i += 9) {
+        if (Sum(i) == 10) {
+            P.push_back(i);
+            j++;
+        }
+    }
+}
+int main () {
+    fun();
+    ll k;
+    while (scanf("%lld", &k) == 1) {
+        printf("%lld\n", P[k - 1]);
+    }
+    return 0;
+}
